76548abcefab84dc56f07cc165d3223928a41d4a
OmarHoussami <omar.houssami20@gmail.com>
2021-07-18 18:07:40 +0300

    security edits

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //const { mongoPath } = require('./config.json')
 
-const mongoPath = 'mongodb+srv://root:root@server.dcx3b.mongodb.net/discordbot?retryWrites=true&w=majority'
+const mongoPath = ''
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
         useNewUrlParser: true,
